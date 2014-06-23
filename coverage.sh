c478fb32c92c24796f5d25ac974a7f8118ca5239
jrajav <jrajav@gmail.com>
2014-06-23 17:41:14 -0500

    Short-circuiting test scripts on failure

--- coverage.sh
+++ coverage.sh
@@ -9,10 +9,10 @@ cyanprint () {
 rm -rf build
 mkdir build
 
-cyanprint "Instrumenting source files for coverage"
-node_modules/visionmedia-jscoverage/jscoverage src build && cyanprint "Instrumenting complete"
-
-cyanprint "Generating coverage report"
-node_modules/mocha/bin/mocha --reporter html-cov test/test.js > coverage.html
+cyanprint "Instrumenting source files for coverage" &&
+node_modules/visionmedia-jscoverage/jscoverage src build &&
+cyanprint "Instrumenting complete" &&
+cyanprint "Generating coverage report" &&
+node_modules/mocha/bin/mocha --reporter html-cov test/test.js > coverage.html &&
 which -s open && open coverage.html
 cyanprint "Coverage complete"
--- test.sh
+++ test.sh
@@ -10,8 +10,9 @@ rm -rf build
 mkdir build
 cp -R src/* build
 
-cyanprint "Starting ESLint"
-node_modules/eslint/bin/eslint.js . && cyanprint "ESLint complete"
-
-cyanprint "Starting tests"
-node_modules/mocha/bin/mocha --reporter spec test/test.js && cyanprint "Tests complete"
+cyanprint "Starting ESLint" &&
+node_modules/eslint/bin/eslint.js . &&
+cyanprint "ESLint complete" &&
+cyanprint "Starting tests" &&
+node_modules/mocha/bin/mocha --reporter spec test/test.js &&
+cyanprint "Tests complete"
